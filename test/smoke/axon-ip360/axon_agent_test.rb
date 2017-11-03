b4107bf2a6e54a65e3801f29460ea755b04eef1f
RHanson <rhanson@tripwire.com>
2017-11-03 13:57:55 -0700

    Allowing for co-existing Axon agents
    == STORY / BUGS
    
    Resolves #8
    Resolves #9
    
    == DETAILS
    
    Enhancing the cookbook to allow users to set the service name for
    the IP360 Axon agent as well as set its configuration directory
    path.
    
    Minor fixes to the custom resource to validate co-existing agents
    can be installed by a wrapper cookbook.
    
    == TESTING
    
    kitchen test
    foodctiric
    cookstyle

---
+++ test/smoke/axon-ip360/axon_agent_test.rb
@@ -0,0 +1,58 @@
+# # encoding: utf-8
+
+# Inspec test for recipe tripwire_agent::axon_agent
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+if os.windows?
+  config_dir = 'C:\ProgramData\Tripwire\agent\config'
+  config_file = config_dir + '\twagent.conf'
+  reg_key_file = config_dir + '\registration_pre_shared_key.txt'
+  pkg_name = 'tw-axon-agent-ip360'
+  srvc_nme = 'Tripwire Axon Agent for IP360'
+else
+  config_dir = '/etc/tripwire-ip360'
+  config_file = config_dir + '/twagent.conf'
+  reg_key_file = config_dir + '/registration_pre_shared_key.txt'
+  pkg_name = 'tw-axon-agent-ip360'
+  srvc_nme = 'tw-axon-agent-ip360'
+end
+
+# Axon config directory should exist
+describe directory(config_dir) do
+  it { should exist }
+end
+
+# Configuration file should exist and have the bridge, port, and spool size set
+describe file(config_file) do
+  it { should exist }
+  its('content') { should match /bridge\.host=tw-bridge\.example.com/ }
+  its('content') { should match /bridge\.port=5670/ }
+  its('content') { should match /spool.size.max=1g/ }
+
+  # Following should not exist in the file
+  its('content') { should_not match /dns\.service\.name=bridge/ }
+  its('content') { should_not match /dns\.service\.domain=example\.com/ }
+  its('content') { should_not match /socks5\.*/}
+  its('content') { should_not match /tls\.version*/ }
+  its('content') { should_not match /tls\.cipher\.suites*/ }
+end
+
+# Axon agents registration key should exist and contain the test password
+describe file(reg_key_file) do
+  it { should exist }
+  its('content') { should match /123PAs5W0rD/ }
+end
+
+# Axon agent should be installed
+describe package(pkg_name) do
+  it { should be_installed }
+end
+
+# Axon service should be running
+describe service(srvc_nme) do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
